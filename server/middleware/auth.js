cf6bd10c62d98c6ea4e26114d50555e1ff6d2448
Addison Hernandez <addison.hernandez@gmail.com>
2022-03-19 11:47:30 -0400

    Implement createSession functionality

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,10 +1,45 @@
 const models = require('../models');
 const Promise = require('bluebird');
 
+const createNewCookie = function (req, res) {
+  return models.Sessions.create()
+    .then(result => {
+      let id = result.insertId;
+      return models.Sessions.get({ id });
+    })
+    .then(hashObj => {
+      req.session = hashObj;
+      res.cookie('shortlyid', req.session.hash);
+      return hashObj;
+    });
+};
+
 module.exports.createSession = (req, res, next) => {
+  if (req.cookies.shortlyid) {
+    let hash = req.cookies.shortlyid;
+
+    models.Sessions.get({ hash })
+      //sessions already exists
+      .then(result => {
+        if (result) {
+          req.session = result;
+          res.cookie('shortlyid', req.session.hash);
+        } else {
+          createNewCookie(req, res)
+            .then(result => next(null, result))
+            .catch(error => next(error));
+        }
+      })
+      .then(result => next(null, result))
+      .catch(error => next(error));
+
+  } else {
+    createNewCookie(req, res)
+      .then(result => next(null, result))
+      .catch(error => next(error));
+  }
 };
 
 /************************************************************/
 // Add additional authentication middleware functions below
 /************************************************************/
-
