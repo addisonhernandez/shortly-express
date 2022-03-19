cd879e7b9c8ca40d291a4300d8bd91d51a95fc90
Addison Hernandez <addison.hernandez@gmail.com>
2022-03-19 11:48:26 -0400

    Implment cookieParser functionality

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,21 +1,15 @@
-const { Sessions } = require('../models');
-
 const parseCookies = (req, res, next) => {
   let cookieObj = {};
   if (req.headers.cookie) {
-    //TODO: this is better (no %20 or anything), but not quite right yet
     const cookieString = decodeURI(req.headers.cookie);
 
-    let cookieKeyValues = cookieString.split(';').map(eachCookie => {
+    let cookieKeyValues = cookieString.split('; ').map(eachCookie => {
       return eachCookie.split('=');
     });
     cookieObj = Object.fromEntries(cookieKeyValues);
-  } else {
-    // create a new cookie
 
-    // set it to cookieObj
+    req.cookies = cookieObj;
   }
-  res.cookie('test', cookieObj);
 
   next();
 };
