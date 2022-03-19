aabba76e7bbf2bc170ac1384e6b56b82d39c9177
Addison Hernandez <addison.hernandez@gmail.com>
2022-03-18 20:14:34 -0400

    Outline the logical structure of parseCookies

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,23 @@
+const { Sessions } = require('../models');
+
 const parseCookies = (req, res, next) => {
+  let cookieObj = {};
+  if (req.headers.cookie) {
+    //TODO: this is better (no %20 or anything), but not quite right yet
+    const cookieString = decodeURI(req.headers.cookie);
+
+    let cookieKeyValues = cookieString.split(';').map(eachCookie => {
+      return eachCookie.split('=');
+    });
+    cookieObj = Object.fromEntries(cookieKeyValues);
+  } else {
+    // create a new cookie
+
+    // set it to cookieObj
+  }
+  res.cookie('test', cookieObj);
+
+  next();
 };
 
 module.exports = parseCookies;
